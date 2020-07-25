b7c3253f0f89d6408a4836dab81223f8f981c819
Melanie Panem <melanie.panem@creditone.com>
2020-07-24 23:50:14 -0700

    restricting current portfolio to projects that actually came to fruition

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -4,97 +4,97 @@ const portfdescrMils = document.getElementById('portfdescr-mils');
 const portfimgMils = document.getElementById('portfimg-mils');
 const portfdescrBusters = document.getElementById('portfdescr-busters');
 const portfimgBusters = document.getElementById('portfimg-busters');
-const portfdescrRecWidgt = document.getElementById('portfdescr-recwidgit');
-const portfimgRecWidgt = document.getElementById('portfimg-recwidgit');
+// const portfdescrRecWidgt = document.getElementById('portfdescr-recwidgit');
+// const portfimgRecWidgt = document.getElementById('portfimg-recwidgit');
 const portfdescrDetPanem = document.getElementById('portfdescr-detailpanem');
 const portfimgDetPanem = document.getElementById('portfimg-detailpanem');
 const portfdescrRecMobile = document.getElementById('portfdescr-recmobile');
 const portfimgRecMobile = document.getElementById('portfimg-recmobile');
-const portfdescrRecFlow = document.getElementById('portfdescr-userflows');
-const portfimgRecFlow = document.getElementById('portfimg-userflows');
+// const portfdescrRecFlow = document.getElementById('portfdescr-userflows');
+// const portfimgRecFlow = document.getElementById('portfimg-userflows');
 
 
 // do it
-portfimgMils.addEventListener("mouseover", function() {   
+portfimgMils.addEventListener("mouseover", function () {
   this.classList.add('anim');
 }, false);
-portfimgMils.addEventListener("mouseout", function() {   
+portfimgMils.addEventListener("mouseout", function () {
   this.classList.remove('anim');
 }, false);
 
-portfdescrMils.addEventListener("mouseover", function() {   
+portfdescrMils.addEventListener("mouseover", function () {
   this.classList.add('anim');
 }, false);
-portfdescrMils.addEventListener("mouseout", function() {   
+portfdescrMils.addEventListener("mouseout", function () {
   this.classList.remove('anim');
 }, false);
 
-portfimgBusters.addEventListener("mouseover", function() {   
+portfimgBusters.addEventListener("mouseover", function () {
   this.classList.add('anim');
 }, false);
-portfimgBusters.addEventListener("mouseout", function() {   
+portfimgBusters.addEventListener("mouseout", function () {
   this.classList.remove('anim');
 }, false);
 
-portfdescrBusters.addEventListener("mouseover", function() {   
+portfdescrBusters.addEventListener("mouseover", function () {
   this.classList.add('anim');
 }, false);
-portfdescrBusters.addEventListener("mouseout", function() {   
+portfdescrBusters.addEventListener("mouseout", function () {
   this.classList.remove('anim');
 }, false);
 
-portfimgRecWidgt.addEventListener("mouseover", function() {   
+// portfimgRecWidgt.addEventListener("mouseover", function() {   
+//   this.classList.add('anim');
+// }, false);
+// portfimgRecWidgt.addEventListener("mouseout", function() {   
+//   this.classList.remove('anim');
+// }, false);
+
+// portfdescrRecWidgt.addEventListener("mouseover", function() {   
+//   this.classList.add('anim');
+// }, false);
+// portfdescrRecWidgt.addEventListener("mouseout", function() {   
+//   this.classList.remove('anim');
+// }, false);
+
+portfimgDetPanem.addEventListener("mouseover", function () {
   this.classList.add('anim');
 }, false);
-portfimgRecWidgt.addEventListener("mouseout", function() {   
+portfimgDetPanem.addEventListener("mouseout", function () {
   this.classList.remove('anim');
 }, false);
 
-portfdescrRecWidgt.addEventListener("mouseover", function() {   
+portfdescrDetPanem.addEventListener("mouseover", function () {
   this.classList.add('anim');
 }, false);
-portfdescrRecWidgt.addEventListener("mouseout", function() {   
+portfdescrDetPanem.addEventListener("mouseout", function () {
   this.classList.remove('anim');
 }, false);
 
-portfimgDetPanem.addEventListener("mouseover", function() {   
+portfimgRecMobile.addEventListener("mouseover", function () {
   this.classList.add('anim');
 }, false);
-portfimgDetPanem.addEventListener("mouseout", function() {   
+portfimgRecMobile.addEventListener("mouseout", function () {
   this.classList.remove('anim');
 }, false);
 
-portfdescrDetPanem.addEventListener("mouseover", function() {   
+portfdescrRecMobile.addEventListener("mouseover", function () {
   this.classList.add('anim');
 }, false);
-portfdescrDetPanem.addEventListener("mouseout", function() {   
+portfdescrRecMobile.addEventListener("mouseout", function () {
   this.classList.remove('anim');
 }, false);
 
-portfimgRecMobile.addEventListener("mouseover", function() {   
-  this.classList.add('anim');
-}, false);
-portfimgRecMobile.addEventListener("mouseout", function() {   
-  this.classList.remove('anim');
-}, false);
+// portfimgRecFlow.addEventListener("mouseover", function() {   
+//   this.classList.add('anim');
+// }, false);
+// portfimgRecFlow.addEventListener("mouseout", function() {   
+//   this.classList.remove('anim');
+// }, false);
 
-portfdescrRecMobile.addEventListener("mouseover", function() {   
-  this.classList.add('anim');
-}, false);
-portfdescrRecMobile.addEventListener("mouseout", function() {   
-  this.classList.remove('anim');
-}, false);
-
-portfimgRecFlow.addEventListener("mouseover", function() {   
-  this.classList.add('anim');
-}, false);
-portfimgRecFlow.addEventListener("mouseout", function() {   
-  this.classList.remove('anim');
-}, false);
-
-portfdescrRecFlow.addEventListener("mouseover", function() {   
-  this.classList.add('anim');
-}, false);
-portfdescrRecFlow.addEventListener("mouseout", function() {   
-  this.classList.remove('anim');
-}, false);
+// portfdescrRecFlow.addEventListener("mouseover", function() {   
+//   this.classList.add('anim');
+// }, false);
+// portfdescrRecFlow.addEventListener("mouseout", function() {   
+//   this.classList.remove('anim');
+// }, false);
