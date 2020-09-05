4d5d78e95aa9104a44edaac50867e079ecc88703
Melanie Panem <melanie.panem@creditone.com>
2020-09-05 13:00:51 -0700

    adding hover colors to the desktop version of my site

--- assets/js/color-links.js
+++ assets/js/color-links.js
@@ -3,6 +3,8 @@ var dkbgLinks = document.querySelectorAll('.dkbg-link'), i;
 var ltbgLinks = document.querySelectorAll('.ltbg-link'), j;
 var dkbgBorder = document.querySelectorAll('.dkbg-border'), k;
 var imgsAll = document.querySelectorAll('img');
+var ltbgAll = document.querySelectorAll('.ltbg-link');
+var dkbgAll = document.querySelectorAll('.dkbg-link');
 var portfDescrAll = document.querySelectorAll('div.portfolio-descr');
 // const dkbgClassList = ['pink', 'orange', 'teal', 'green', 'yellow', 'blue', 'purple'];
 // const ltbgClassList = ['purple', 'blue', 'pink', 'rylblue', 'orange', 'teal', 'green'];
@@ -20,24 +22,78 @@ const saturatePrtfDescr = () => {
 		el.classList.add("saturate");
 	})
 }
+const colorLtBgLinks = () => {
+	[].forEach.call(ltbgAll, el => {
+		el.classList.add("color");
+	})
+}
+const colorDkBgLinks = () => {
+	[].forEach.call(dkbgAll, el => {
+		el.classList.add("color");
+	})
+}
 const saturateAll = () => {
 	saturateImgs();
 	saturatePrtfDescr();
+	colorLtBgLinks();
+	colorDkBgLinks();
+}
+const desaturateImgs = () => {
+	[].forEach.call(imgsAll, el => {
+		el.classList.remove("saturate");
+	})
+}
+const desaturatePrtfDescr = () => {
+	[].forEach.call(portfDescrAll, el => {
+		el.classList.remove("saturate");
+	})
+}
+const decolorLtBgLinks = () => {
+	[].forEach.call(ltbgAll, el => {
+		el.classList.remove("color");
+	})
+}
+const decolorDkBgLinks = () => {
+	[].forEach.call(dkbgAll, el => {
+		el.classList.remove("color");
+	})
+}
+const desaturateAll = () => {
+	desaturateImgs();
+	desaturatePrtfDescr();
+	decolorLtBgLinks();
+	decolorDkBgLinks();
 }
 const hoverPrtfDescr = () => {
 	[].forEach.call(portfDescrAll, el => {
-		el.addEventListener("hover", saturateAll);
+		el.addEventListener("mouseover", saturateAll);
+		el.addEventListener("mouseout", desaturateAll);
 	})
 }
 const hoverImgs = () => {
 	[].forEach.call(imgsAll, el => {
-		el.addEventListener("hover", saturateAll);
+		el.addEventListener("mouseover", saturateAll);
+		el.addEventListener("mouseout", desaturateAll);
+	})
+}
+const hoverLtBg = () => {
+	[].forEach.call(ltbgAll, el => {
+		el.addEventListener("mouseover", saturateAll);
+		el.addEventListener("mouseout", desaturateAll);
+	})
+}
+const hoverDkBg = () => {
+	[].forEach.call(dkbgAll, el => {
+		el.addEventListener("mouseover", saturateAll);
+		el.addEventListener("mouseout", desaturateAll);
 	})
 }
 
 window.addEventListener("load", function () {
 	hoverPrtfDescr();
 	hoverImgs();
+	hoverLtBg();
+	hoverDkBg();
 });
 
 // plan it
