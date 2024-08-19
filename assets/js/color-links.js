928e4923cabd1527fd538edd56e3514e57105989
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2024-08-19 16:37:04 -0700

    tested button color animation, removed synced link color animation from all pages except index, and added in portfolio page cards

--- assets/js/color-links.js
+++ assets/js/color-links.js
@@ -1,52 +1,38 @@
 // declare it
-var dkbgLinks = document.querySelectorAll('.dkbg-link'), i;
-var ltbgLinks = document.querySelectorAll('.ltbg-link'), j;
-var dkbgBorder = document.querySelectorAll('.dkbg-border'), k;
-var imgsAll = document.querySelectorAll('img');
+// var dkbgLinks = document.querySelectorAll('.dkbg-link'), i;
+// var ltbgLinks = document.querySelectorAll('.ltbg-link'), j;
+// var bgBtns = document.querySelectorAll('a.button'), k;
+// var dkbgBorder = document.querySelectorAll('.dkbg-border'), l;
+// var imgsAll = document.querySelectorAll('img');
 var ltbgAll = document.querySelectorAll('.ltbg-link');
 var dkbgAll = document.querySelectorAll('.dkbg-link');
-var portfDescrAll = document.querySelectorAll('div.portfolio-descr');
-// const dkbgClassList = ['pink', 'orange', 'teal', 'green', 'yellow', 'blue', 'purple'];
-// const ltbgClassList = ['purple', 'blue', 'pink', 'rylblue', 'orange', 'teal', 'green'];
-// let dkbgColorClass = "";
-// let ltbgColorClass = "";
+var bgBtnsAll = document.querySelectorAll('.button');
+// console.log('sees color-links.js file');
 
 
-const saturateImgs = () => {
-	[].forEach.call(imgsAll, el => {
-		el.classList.add("saturate");
-	})
-}
-const saturatePrtfDescr = () => {
-	[].forEach.call(portfDescrAll, el => {
-		el.classList.add("saturate");
-	})
-}
 const colorLtBgLinks = () => {
 	[].forEach.call(ltbgAll, el => {
 		el.classList.add("color");
+		console.log('ltbg link');
 	})
 }
 const colorDkBgLinks = () => {
 	[].forEach.call(dkbgAll, el => {
 		el.classList.add("color");
+		console.log('dkbg link');
 	})
+}
+const colorBtnBgLinks = () => {
+	[].forEach.call(bgBtnsAll, el => {
+		el.classList.add("bgcolor");
+		console.log('should add class bgcolor here');
+	})
+	console.log('sees function');
 }
 const saturateAll = () => {
-	saturateImgs();
-	saturatePrtfDescr();
 	colorLtBgLinks();
 	colorDkBgLinks();
-}
-const desaturateImgs = () => {
-	[].forEach.call(imgsAll, el => {
-		el.classList.remove("saturate");
-	})
-}
-const desaturatePrtfDescr = () => {
-	[].forEach.call(portfDescrAll, el => {
-		el.classList.remove("saturate");
-	})
+	colorBtnBgLinks();
 }
 const decolorLtBgLinks = () => {
 	[].forEach.call(ltbgAll, el => {
@@ -58,72 +44,39 @@ const decolorDkBgLinks = () => {
 		el.classList.remove("color");
 	})
 }
+const decolorBtnBgLinks = () => {
+	[].forEach.call(bgBtnsAll, el => {
+		el.classList.remove("bgcolor");
+		console.log('should remove class bgcolor here');
+	})
+}
 const desaturateAll = () => {
-	desaturateImgs();
-	desaturatePrtfDescr();
 	decolorLtBgLinks();
 	decolorDkBgLinks();
-}
-const hoverPrtfDescr = () => {
-	[].forEach.call(portfDescrAll, el => {
-		el.addEventListener("mouseover", saturateAll);
-		el.addEventListener("mouseout", desaturateAll);
-	})
-}
-const hoverImgs = () => {
-	[].forEach.call(imgsAll, el => {
-		el.addEventListener("mouseover", saturateAll);
-		el.addEventListener("mouseout", desaturateAll);
-	})
+	decolorBtnBgLinks();
 }
 const hoverLtBg = () => {
 	[].forEach.call(ltbgAll, el => {
-		el.addEventListener("mouseover", saturateAll);
-		el.addEventListener("mouseout", desaturateAll);
+		el.addEventListener("mouseover", colorLtBgLinks);
+		el.addEventListener("mouseout", decolorLtBgLinks);
 	})
 }
 const hoverDkBg = () => {
 	[].forEach.call(dkbgAll, el => {
-		el.addEventListener("mouseover", saturateAll);
-		el.addEventListener("mouseout", desaturateAll);
+		el.addEventListener("mouseover", colorDkBgLinks);
+		el.addEventListener("mouseout", decolorDkBgLinks);
+	})
+}
+const hoverBtnBgLinks = () => {
+	[].forEach.call(bgBtnsAll, el => {
+		el.addEventListener("mouseover", colorBtnBgLinks);
+		el.addEventListener("mouseout", decolorBtnBgLinks);
 	})
 }
 
-window.addEventListener("load", function () {
-	hoverPrtfDescr();
-	hoverImgs();
-	hoverLtBg();
-	hoverDkBg();
-});
-
-// plan it
-// function dkbgRandom() {
-// 	dkbgColorClass = dkbgClassList[Math.floor(Math.random() * dkbgClassList.length)];
-// 	return dkbgColorClass;
-// }
-// function ltbgRandom() {
-// 	ltbgColorClass = ltbgClassList[Math.floor(Math.random() * ltbgClassList.length)];
-// 	return ltbgColorClass;
-// }
-
-// do it
-
-// window.addEventListener("load", function() {
-// 	for (i = 0; i < dkbgLinks.length; ++i) {
-	//   dkbgRandom();
-	//   dkbgLinks[i].classList.add(dkbgColorClass);
-// 	}
-// });
-// window.addEventListener("load", function() {
-// 	for (j = 0; j < ltbgLinks.length; ++j) {
-	//   ltbgRandom();
-	//   ltbgLinks[j].classList.add(ltbgColorClass);
-// 	}
-// });
-// window.addEventListener("load", function() {
-// 	for (j = 0; j < dkbgBorder.length; ++j) {
-// 	  dkbgRandom();
-// 	  dkbgBorder[j].classList.add(dkbgColorClass);
-// 	}
+// window.addEventListener("load", function () {
+// 	hoverLtBg();
+// 	hoverDkBg();
+// 	hoverBtnBgLinks();
 // });
 
