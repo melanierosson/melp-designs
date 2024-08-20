4929cecb8e3b4eda1fc597c3ae3057d60aa47ad4
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2024-08-20 16:48:26 -0700

    modal window pop ups for full size image zooms on the portfolio pages

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,24 +1,65 @@
-const modal = document.getElementById("myModal");
-const imgsMAll = document.querySelectorAll("img.gallery");
-const mimgsAll = document.querySelectorAll("img.modal-content");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-const removeDisplay = () => {
-    [].forEach.call(mimgsAll, el => {
-        el.classList.remove("show-modal");
-    });
+// Get the modal
+var modal = document.getElementById('myModal');
+ 
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var bceImg1 = document.getElementById('bce-img-1');
+var modalBceImg1 = document.getElementById("bceimg");
+var bceImg2 = document.getElementById('bce-img-2');
+var bceImg3 = document.getElementById('bce-img-3');
+var bceImg4 = document.getElementById('bce-img-4');
+var bceImg5 = document.getElementById('bce-img-5');
+var bceImg6 = document.getElementById('bce-img-6');
+var captionText = document.getElementById("caption");
+bceImg1.onclick = function(){
+    modal.style.display = "flex";
+    modalBceImg1.style.display = "flex";
+    modalBceImg1.src = this.src;
+    modalBceImg1.alt = this.alt;
+    captionText.innerHTML = this.alt;
 }
-
-imgsMAll.forEach((el, i) => {
-    el.addEventListener('click', () => {
-        modal.style.display = "block";
-        removeDisplay();
-        mimgsAll[i].classList.add('show-modal');
-        mimgsAll[i].src = el.src;
-    });
-})
-// click anywhere on the modal to close it
+bceImg2.onclick = function(){
+    modal.style.display = "flex";
+    modalBceImg1.style.display = "flex";
+    modalBceImg1.src = this.src;
+    modalBceImg1.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+bceImg3.onclick = function(){
+    modal.style.display = "flex";
+    modalBceImg1.style.display = "flex";
+    modalBceImg1.src = this.src;
+    modalBceImg1.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+bceImg4.onclick = function(){
+    modal.style.display = "flex";
+    modalBceImg1.style.display = "flex";
+    modalBceImg1.src = this.src;
+    modalBceImg1.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+bceImg5.onclick = function(){
+    modal.style.display = "flex";
+    modalBceImg1.style.display = "flex";
+    modalBceImg1.src = this.src;
+    modalBceImg1.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+bceImg6.onclick = function(){
+    modal.style.display = "flex";
+    modalBceImg1.style.display = "flex";
+    modalBceImg1.src = this.src;
+    modalBceImg1.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+ 
+ 
+// When the user clicks on <span> (x), close the modal
 modal.onclick = function() {
-  modal.style.display = "none";
-};
+    modalBceImg1.className += " out";
+    setTimeout(function() {
+       modal.style.display = "none";
+       modalBceImg1.className = "modal-content";
+     }, 400);
+    
+ }  
