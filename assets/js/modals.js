1929e8778a68b3d74cf12fc80baec113b700c642
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2024-08-23 13:27:34 -0700

    working on the auto store case study page and updated responsive layout for portfolio case study pages on bootstrap md size

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -3,7 +3,7 @@ var modal = document.getElementById('myModal');
  
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var bceImg1 = document.getElementById('bce-img-1');
-var modalBceImg1 = document.getElementById("bceimg");
+var modalImg = document.getElementById("modal-img");
 var bceImg2 = document.getElementById('bce-img-2');
 var bceImg3 = document.getElementById('bce-img-3');
 var bceImg4 = document.getElementById('bce-img-4');
@@ -13,61 +13,61 @@ var bceImg7 = document.getElementById('bce-img-7');
 var captionText = document.getElementById("caption");
 bceImg1.onclick = function(){
     modal.style.display = "flex";
-    modalBceImg1.style.display = "flex";
-    modalBceImg1.src = this.src;
-    modalBceImg1.alt = this.alt;
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
     captionText.innerHTML = this.alt;
 }
 bceImg2.onclick = function(){
     modal.style.display = "flex";
-    modalBceImg1.style.display = "flex";
-    modalBceImg1.src = this.src;
-    modalBceImg1.alt = this.alt;
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
     captionText.innerHTML = this.alt;
 }
 bceImg3.onclick = function(){
     modal.style.display = "flex";
-    modalBceImg1.style.display = "flex";
-    modalBceImg1.src = this.src;
-    modalBceImg1.alt = this.alt;
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
     captionText.innerHTML = this.alt;
 }
 bceImg4.onclick = function(){
     modal.style.display = "flex";
-    modalBceImg1.style.display = "flex";
-    modalBceImg1.src = this.src;
-    modalBceImg1.alt = this.alt;
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
     captionText.innerHTML = this.alt;
 }
 bceImg5.onclick = function(){
     modal.style.display = "flex";
-    modalBceImg1.style.display = "flex";
-    modalBceImg1.src = this.src;
-    modalBceImg1.alt = this.alt;
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
     captionText.innerHTML = this.alt;
 }
 bceImg6.onclick = function(){
     modal.style.display = "flex";
-    modalBceImg1.style.display = "flex";
-    modalBceImg1.src = this.src;
-    modalBceImg1.alt = this.alt;
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
     captionText.innerHTML = this.alt;
 }
 bceImg7.onclick = function(){
     modal.style.display = "flex";
-    modalBceImg1.style.display = "flex";
-    modalBceImg1.src = this.src;
-    modalBceImg1.alt = this.alt;
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
     captionText.innerHTML = this.alt;
 }
  
  
 // When the user clicks on <span> (x), close the modal
 modal.onclick = function() {
-    modalBceImg1.className += " out";
+    modalImg.className += " out";
     setTimeout(function() {
        modal.style.display = "none";
-       modalBceImg1.className = "modal-content";
+       modalImg.className = "modal-content";
      }, 400);
     
  }  
