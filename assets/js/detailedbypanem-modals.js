deb45eced7c5a6d7829dda16664fe52b0eac3fa1
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2024-08-29 00:08:39 -0700

    added in rough draft of ecommerce portfolio project page

--- assets/js/detailedbypanem-modals.js
+++ assets/js/detailedbypanem-modals.js
@@ -13,6 +13,19 @@ var autoImg7 = document.getElementById('auto-img-7');
 var autoImg8 = document.getElementById('auto-img-8');
 var autoImg9 = document.getElementById('auto-img-9');
 var autoImg10 = document.getElementById('auto-img-10');
+var autoImg11 = document.getElementById('auto-img-11');
+var autoImg12 = document.getElementById('auto-img-12');
+var autoImg13 = document.getElementById('auto-img-13');
+var autoImg14 = document.getElementById('auto-img-14');
+var autoImg15 = document.getElementById('auto-img-15');
+var autoImg16 = document.getElementById('auto-img-16');
+var autoImg17 = document.getElementById('auto-img-17');
+var autoImg18 = document.getElementById('auto-img-18');
+var autoImg19 = document.getElementById('auto-img-19');
+var autoImg20 = document.getElementById('auto-img-20');
+var autoImg21 = document.getElementById('auto-img-21');
+var autoImg22 = document.getElementById('auto-img-22');
+var autoImg23 = document.getElementById('auto-img-23');
 var captionText = document.getElementById("caption");
 autoImg1.onclick = function(){
     modal.style.display = "flex";
@@ -78,6 +91,97 @@ autoImg9.onclick = function(){
     captionText.innerHTML = this.alt;
 }
 autoImg10.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg11.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg12.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg13.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg14.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg15.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg16.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg17.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg18.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg19.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg20.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg21.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg22.onclick = function(){
+    modal.style.display = "flex";
+    modalImg.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+autoImg23.onclick = function(){
     modal.style.display = "flex";
     modalImg.style.display = "flex";
     modalImg.src = this.src;
